09ca6fb11b6aa08b12183f49f34af3e2b817c92c
Amit-Patra-04 <k2amitpatra@gmail.com>
2025-08-06 18:20:41 +0530

    Create Dots

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ let ctx = canvas.getContext('2d');
 
 let dots = [];
 let arrayColors = ['#960200', '#FFD23F', '#13293D', '#585123', '#47A025'];
+
+// Create 50 random dots with properties
+for (let index = 0; index < 50; index++) {
+    dots.push({
+        x: Math.floor(Math.random() * canvas.width),
+        y: Math.floor(Math.random() * canvas.height),
+        size: Math.random() * 3 + 5,
+        color: arrayColors[Math.floor(Math.random() * 5)]
+    });
+}
